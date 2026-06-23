beea2dd74f6da1c70e8da11aff01ab2535db79bb
tom-biotech <tomlorenz51@gmail.com>
2026-06-23 11:33:42 +0200

    Update WGS_allelfreq.sh

--- Scripts/WGS_allelfreq.sh
+++ Scripts/WGS_allelfreq.sh
@@ -1,35 +1,28 @@
 #!/bin/sh
 
 ############# VARROARESISTENZ WGS 2026 ################ 
+## using grenedalf 0.6.3
 
-# create all_bams.list
-ref_dir="/home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2"
-bam_dir="/home/tomsch/WGS_36/aligned"
-mkdir -p "/home/tomsch/WGS_36/vcfs"
-vcf_dir="/home/tomsch/WGS_36/vcfs"
+aligned_dir="/home/tomsch/WGS_36/aligned"
+mkdir -p /home/tomsch/WGS_36/aligned/allele_freq
+allel_freq_dir="/home/tomsch/WGS_36/aligned/allele_freq"
+genome="/home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna"
 
-##create all_bams.list and chromosomes.txt
-find $bam_dir -name "*.bam" > $bam_dir/all_bams.list
-cut -f 1 $ref_dir/GCF_003254395.2_Amel_HAv3.1_genomic.fna.fai > $ref_dir/chromosomes.txt
-
-# Run mpileup per chromosome using parallel 
-cat $ref_dir/chromosomes.txt | parallel -j 40 \
-    "bcftools mpileup -Ou -f $ref_dir/GCF_003254395.2_Amel_HAv3.1_genomic.fna \
-    -b $bam_dir/all_bams.list \
-    -r {} -q 20 -Q 30 -d XXXXX -a FORMAT/DP,FORMAT/AD \
-    -o $vcf_dir/raw_mpileup_{}.bcf"
-    
-## and run variant calling separately for each chromosome
-for i in $(cat $ref_dir/chromosomes.txt)
-do name=$(basename ${i})
-    echo "processing $name"
-bcftools call -mv -v -Oz --threads 20 -o $vcf_dir/${name}_raw_snps.vcf.gz $vcf_dir/raw_mpileup_${name}.bcf
+for i in "$aligned_dir"/*_rmd.bam;
+do dname=$(dirname ${i}); name=$(basename ${i} _rmd.bam); 
+/home/tomsch/grenedalf/bin/grenedalf frequency \
+--sam-path "$aligned_dir"/${name}_rmd.bam \
+--sam-min-map-qual 60 \
+--sam-min-base-qual 30 \
+--reference-genome-fasta "$genome" \
+--out-dir "$allele_freq_dir" \
+--file-prefix ${name}_ \
+--compress \
+--threads 20 \
+--log-file "$allele_freq_dir"/${name}.log \
+--write-sample-counts \
+--write-sample-read-depth \
+--write-sample-alt-freq \
+--separator-char tab; 
 done
 
-## merge all vcfs from different chromosomes
-ls -1 $vcf_dir/*_raw_snps.vcf.gz > $vcf_dir/vcf_list.txt
-bcftools concat -Oz $vcf_dir/vcf_list.txt -o $vcf_dir/wgs_36_raw_snp.vcf.gz
-tabix -p vcf $vcf_dir/wgs_36_raw_snp.vcf.gz
-
-## check summary information stats from vcf file
-bcftools stats  -s - $vcf_dir/wgs_36_raw_snp.vcf.gz > $vcf_dir/wgs_36_raw_snp.sumstats
