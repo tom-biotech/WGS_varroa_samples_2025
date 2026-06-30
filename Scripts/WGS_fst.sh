f7116460813181316cc7ad5e418bb867e6e79ebc
tom-biotech <tomlorenz51@gmail.com>
2026-06-30 11:19:17 +0200

    Update WGS_fst.sh

--- Scripts/WGS_fst.sh
+++ Scripts/WGS_fst.sh
@@ -6,7 +6,7 @@
 # first of all we have to check which of the B211 samples is the true one
 # so we calculate the fst for samples B11(TGW)24 (B5047-SCH-44), B211T(TGW)24 (B5047-SCH-53) and B211/2(TGW)24 (B5047-SCH-54). B211T(TGW)24 is used as the control,
 # because we know that this is the true B211(TGW)24, but DNA was extracted from thorax instead as from heads
-grenedalf fst \
+/home/tomsch/grenedalf/bin/grenedalf fst \
 --window-type genome \
 --method unbiased-hudson\
 --frequency-table-path /home/tomsch/WGS_36/aligned/allele_freq_2/all_samplesfrequency.csv.gz \
