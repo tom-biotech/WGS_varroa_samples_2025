25e10203919e49a8d13ed35b2d2a1243800daf8f
tom-biotech <tomlorenz51@gmail.com>
2026-07-04 15:41:43 +0200

    Update WGS_fst.sh

--- Scripts/WGS_fst.sh
+++ Scripts/WGS_fst.sh
@@ -26,10 +26,10 @@
 #############################################################
 
 # from samtools mpileup to sync file
-for i in B5047-SCH-{44,53,54}_rmd.bam; do name=$(basename ${i} _rmd.bam);
+for i in B5047-SCH-{25-60}_rmd.bam; do name=$(basename ${i} _rmd.bam);
 cat /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/chromosomes.txt | parallel -j 8 \
 "samtools mpileup -B -f /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna \
--q 40 -Q 20 -r {} ${i} > mpileup_files/${name}_{}.mpileup"
+-q 40 -Q 20 -aa -r {} ${i} > mpileup_files/${name}_{}.mpileup"
 while read c; do cat mpileup_files/${name}_${c}.mpileup; done < /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/chromosomes.txt > mpileup_files/${name}.mpileup
 java -ea -Xmx10g -jar \
 /home/tomsch/miniconda3/envs/WGS_36/share/popoolation2-1.201-0/mpileup2sync.jar --input mpileup_files/${name}.mpileup --output mpileup_files/${name}.sync --fastq-type sanger --min-qual 20 --threads 10;
@@ -60,6 +60,11 @@ done
 
 # just for the mitochondrium 
 # MeanDepth = 24199, Sd = 5010 -> so for Depth filter use mean +- 2*Sd -> 34219 - 14179
+###### ATTENTION!!!!!! MeanDepth was calculated with PanDepth and the raw *rmd.bam. (just the -q from fastp was taken into account), 
+###### but now the reads are filtert for -q 40 and -Q 20 from samools mpileup. MeanDepth is now different und has to be calculated from 
+###### the sync files !!!!!
+
+
 
 /home/tomsch/grenedalf/bin/grenedalf fst \
 --method unbiased-hudson \
