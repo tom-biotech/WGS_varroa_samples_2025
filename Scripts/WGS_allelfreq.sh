08e3fc81023b8f73df3bb7e2728169bb3fe3d0a2
tom-biotech <tomlorenz51@gmail.com>
2026-07-04 10:09:38 +0200

    Update WGS_allelfreq.sh

--- Scripts/WGS_allelfreq.sh
+++ Scripts/WGS_allelfreq.sh
@@ -9,15 +9,13 @@ allel_freq_dir="/home/tomsch/WGS_36/aligned/allele_freq"
 genome="/home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna"
 
 /home/tomsch/grenedalf/bin/grenedalf frequency \
---sam-path B5047-SCH-25_rmd.bam  B5047-SCH-29_rmd.bam  B5047-SCH-33_rmd.bam  B5047-SCH-37_rmd.bam  B5047-SCH-41_rmd.bam  B5047-SCH-45_rmd.bam  B5047-SCH-49_rmd.bam  B5047-SCH-53_rmd.bam  B5047-SCH-57_rmd.bam B5047-SCH-26_rmd.bam  B5047-SCH-30_rmd.bam  B5047-SCH-34_rmd.bam  B5047-SCH-38_rmd.bam  B5047-SCH-42_rmd.bam  B5047-SCH-46_rmd.bam  B5047-SCH-50_rmd.bam  B5047-SCH-54_rmd.bam  B5047-SCH-58_rmd.bam B5047-SCH-27_rmd.bam  B5047-SCH-31_rmd.bam  B5047-SCH-35_rmd.bam  B5047-SCH-39_rmd.bam  B5047-SCH-43_rmd.bam  B5047-SCH-47_rmd.bam  B5047-SCH-51_rmd.bam  B5047-SCH-55_rmd.bam  B5047-SCH-59_rmd.bam B5047-SCH-28_rmd.bam  B5047-SCH-32_rmd.bam  B5047-SCH-36_rmd.bam  B5047-SCH-40_rmd.bam  B5047-SCH-44_rmd.bam  B5047-SCH-48_rmd.bam  B5047-SCH-52_rmd.bam  B5047-SCH-56_rmd.bam  B5047-SCH-60_rmd.bam  \
---sam-min-map-qual 60 \
---sam-min-base-qual 30 \
+--sync-path sync_files/B5047-SCH-44.sync  sync_files/B5047-SCH-53.sync  sync_files/B5047-SCH-54.sync \
 --reference-genome-fasta /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna \
---out-dir allele_freq_2 \
---file-prefix all_samples \
+--out-dir b211_allele_freq \
+--file-prefix b211_samples \
 --compress \
---threads 15 \
---log-file /home/tomsch/WGS_36/aligned/allele_freq_2/all_samples_grenedalf_freq.log \
+--threads 20 \
+--log-file /home/tomsch/WGS_36/aligned/b211_allele_freq/b211_samples_grenedalf_freq.log \
 --write-sample-counts \
 --write-sample-read-depth \
 --write-sample-alt-freq \
