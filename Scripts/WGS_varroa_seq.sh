a4bf6c4924a82e51e7ab0431f1e536cdac713c97
tom-biotech <tomlorenz51@gmail.com>
2026-07-08 14:26:47 +0200

    Update WGS_varroa_seq.sh

--- Scripts/WGS_varroa_seq.sh
+++ Scripts/WGS_varroa_seq.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+############# VARROARESISTENZ WGS 2026 ################ 
+#### using bwa-mem2 2.3
+#### using samtools 1.23.1
+#### using pandepth 2.26
+#### using alfred 0.5.3
+
+
+bam_dir="/home/tomsch/WGS_36/aligned"
+mkdir -p "/home/tomsch/WGS_36/unmapped"
+unmap_dir="/home/tomsch/WGS_36/unmapped"
+
+# using the unmapped reads from mapping against the Amel_HAv3.1 to mapp against mito genome of Varroa destructor
+
+# extract unmapped reads with samtools
+
+for i in $aligned/*_rmd.bam;
+do name=$(basename $i _rmd.bam);
+
+samtools view -b -f 4 -@ 20 $i -o $unmap_dir/${name}_unmapped.bam
+
+done
