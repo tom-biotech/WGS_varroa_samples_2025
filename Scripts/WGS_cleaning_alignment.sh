3ee6deff002022cdd37ac06c8a9e59fa690a9364
tom-biotech <tomlorenz51@gmail.com>
2026-07-09 21:54:06 +0200

    Update WGS_cleaning_alignment.sh

--- Scripts/WGS_cleaning_alignment.sh
+++ Scripts/WGS_cleaning_alignment.sh
@@ -1,5 +1,5 @@
 
-#!/bin/sh
+#!/bin/bash
 
 ############# VARROARESISTENZ WGS 2026 ################ 
 #### using bbmap 39.85
@@ -9,20 +9,20 @@
 #### using alfred 0.5.3
 
 fastq_raw="/drives/HDD_22TB_RAWDATA/20260605_Genewiz_Varroaresistenz/00_fastq"
-mkdir -p "/home/tomsch/WGS_36/Clean"
-fastq_clean="/home/tomsch/WGS_36/Clean"
+mkdir -p "/home/tomsch/WGS_36/Clean_new"
+fastq_clean="/home/tomsch/WGS_36/Clean_new"
 genome="/home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna"
-mkdir -p "/home/tomsch/WGS_36/QC"
-qc_dir="/home/tomsch/WGS_36/QC"
+mkdir -p "/home/tomsch/WGS_36/QC_new"
+qc_dir="/home/tomsch/WGS_36/QC_new"
 prop_aligned="$qc_dir/counts_summary_all_samples.txt"
-mkdir -p "/home/tomsch/WGS_36/aligned"
-aligned_dir="/home/tomsch/WGS_36/aligned"
+mkdir -p "/home/tomsch/WGS_36/aligned_new"
+aligned_dir="/home/tomsch/WGS_36/aligned_new"
 
 ## adapter and low quality base trimming
 for i in "$fastq_raw"/*_R1_001.fastq.gz; \
         do dname=$(dirname ${i}); name=$(basename ${i} _R1_001.fastq.gz); \
         fastp -i ${dname}/${name}_R1_001.fastq.gz -I ${dname}/${name}_R2_001.fastq.gz -o $fastq_clean/${name}_clean_1.fastq.gz -O $fastq_clean/${name}_clean_2.fastq.gz \
-		-h $fastq_clean/${name}_stats.html -j $fastq_clean/${name}_stats.json -w 10 --dont_overwrite --detect_adapter_for_pe -q 20 -l 30 --correction --cut_right --cut_mean_quality 20 ;
+		-h $fastq_clean/${name}_stats.html -j $fastq_clean/${name}_stats.json -w 20 --dont_overwrite --detect_adapter_for_pe -q 20 -l 30 --correction ;
 done
 
 ## call the genome (using the curated GCF version), to download here: https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_003254395.2/
@@ -41,11 +41,11 @@ for i in "$fastq_clean"/*_clean_1.fastq.gz
         sorted_bam=$aligned_dir/${name}_aligned_sorted.bam
         rmd_bam=$aligned_dir/${name}_rmd.bam
 		  
-	bwa-mem2 mem -t 30 -R "@RG\tID:${name}\tSM:${name}\tPL:illumina\tLB:lib1\tPU:unit1" $genome $in1 $in2 | samtools view -@ 30 -bSu - > $bam
-	samtools sort -@ 30 -o $sorted_bam $bam
-	samtools index -@ 30 $sorted_bam    
+	bwa-mem2 mem -t 20 -R "@RG\tID:${name}\tSM:${name}\tPL:illumina\tLB:lib1\tPU:unit1" $genome $in1 $in2 | samtools view -@ 20 -bSu - > $bam
+	samtools sort -@ 20 -o $sorted_bam $bam
+	samtools index -@ 20 $sorted_bam    
 	gatk --java-options "-Xmx140G" MarkDuplicates I=$sorted_bam O=$rmd_bam REMOVE_DUPLICATES=true M=$qc_dir/${name}.duplicates.txt
-	samtools index -@ 30 $rmd_bam
+	samtools index -@ 20 $rmd_bam
 	rm -f "$bam"
 	rm -f "$sorted_bam"
 	rm -f "${sorted_bam}".bai
@@ -72,7 +72,7 @@ pandepth -i $i -o "$aligned_dir"/${name}_depth -t 20;
 done
 
 mkdir "$aligned_dir"/bam_qc
-bam_qc_dir="/home/tomsch/WGS_36/aligned/bam_qc"
+bam_qc_dir="/home/tomsch/WGS_36/aligned_new/bam_qc"
 
 ## alfred
 parallel -j 20 '
