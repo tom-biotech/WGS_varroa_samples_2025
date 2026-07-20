00279ad47afcbb60f4cad9d1deb472960d7b7e9b
tom-biotech <tomlorenz51@gmail.com>
2026-07-20 08:31:41 +0200

    Update WGS_Fst.sh

--- Scripts/WGS_Fst.sh
+++ Scripts/WGS_Fst.sh
@@ -31,24 +31,99 @@
 ###### ATTENTION!!!!!! MeanDepth was calculated with PanDepth and the raw *rmd.bam. (just the -q from fastp was taken into account), 
 ###### but now the reads are filtert for -q 40 and -Q 20 from samools mpileup. MeanDepth is now different und has to be calculated from 
 ###### the sync files !!!!!
-###### --> Mean Depth over all samples and Chr = 171.6x = 172x
+
+# Depth in sync files
+
+depth_stats_per_sample.awk
+----------
+BEGIN{OFS="\t"}
+{
+    chrom = $1
+    n = split($4, counts, ":")
+    depth = 0
+    for (i = 1; i <= n; i++) depth += counts[i]
+
+    count[chrom]++
+    delta = depth - mean[chrom]
+    mean[chrom] += delta / count[chrom]
+    delta2 = depth - mean[chrom]
+    M2[chrom] += delta * delta2
+}
+END {
+    for (chrom in count) {
+        n = count[chrom]
+        variance = (n > 1) ? M2[chrom] / (n - 1) : 0
+        sd = sqrt(variance)
+        printf "%s\t%d\t%.4f\t%.4f\n", chrom, n, mean[chrom], sd
+    }
+}
+----------
+# Depth stats per sample and chromosom
+mkdir -p stats_per_sample
+
+ls /home/tomsch/WGS_36/aligned_new/sync_files/*.sync | parallel -j 10 \
+    'sample=$(basename {} .sync); awk -f depth_stats_per_sample.awk {} > stats_per_sample/${sample}.stats'
+
+echo -e "sample\tchrom\tn_positions\tmean_depth\tsd_depth" > depth_stats_final.tsv
+
+for f in stats_per_sample/*.stats; do
+    sample=$(basename "$f" .stats)
+    awk -v s="$sample" 'BEGIN{OFS="\t"} {print s, $0}' "$f"
+done >> stats_per_sample/depth_stats_final.tsv
+
+awk 'NR==FNR{order[$1]=NR; next} FNR>1{print $0, order[$2]}' /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/chromosomes.txt depth_stats_final.tsv \
+    | sort -k6,6n \
+    | cut -d' ' -f1-5 > depth_stats_final_sorted.tsv
+
+# Mean depth per sample
+mean_depth_per_sample.awk:
+-------------
+{
+    n = split($4, counts, ":")
+    depth = 0
+    for (i = 1; i <= n; i++) depth += counts[i]
+
+    count++
+    delta = depth - mean
+    mean += delta / count
+    delta2 = depth - mean
+    M2 += delta * delta2
+
+    if (depth > 0) covered++
+}
+END {
+    variance = (count > 1) ? M2 / (count - 1) : 0
+    sd = sqrt(variance)
+    breadth = (covered / count) * 100
+    printf "%d\t%.4f\t%.4f\t%d\t%.4f\n", count, mean, sd, covered, breadth
+}
+-------------
+
+ls /home/tomsch/WGS_36/aligned_new/sync_files/*.sync | parallel -j 20 \
+    'sample=$(basename {} .sync); echo -e "$sample\t$(awk -f mean_depth_per_sample.awk {})"' \
+    > stats_per_sample/mean_depth_stats_per_sample.tsv
+	
+awk '{ sum += $3; n++ } END { if (n > 0) print sum / n; }' mean_depth_stats_per_sample.tsv 
+
+###### --> Mean Depth over all samples and Chr = 175.604x
 
 /home/tomsch/grenedalf/bin/grenedalf fst 
---method unbiased-hudson 
---window-type genome 
---write-pi-tables 
---sync-path /home/tomsch/WGS_36/aligned/sync_files 
---reference-genome-fasta /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna 
---filter-sample-min-count 2 
---filter-sample-min-read-depth 86 
---filter-sample-max-read-depth 344 
---window-average-policy valid-loci 
---filter-total-snp-min-frequency 0.01 
---pool-sizes 60 
---file-prefix all_samples_fst_calculation_ 
---out-dir /home/tomsch/WGS_36/aligned/fst_files/all_samples 
---compress 
---log-file /home/tomsch/WGS_36/aligned/fst_files/all_samples/all_samples_fst.log 
+--method unbiased-hudson \
+--window-type genome \
+--write-pi-tables \
+--sync-path /home/tomsch/WGS_36/aligned/sync_files \
+--reference-genome-fasta /home/tomsch/WGS_36/Amel_HAv3.1/ncbi_dataset/data/GCF_003254395.2/GCF_003254395.2_Amel_HAv3.1_genomic.fna \
+--filter-sample-min-count 3 \
+--filter-sample-min-read-depth 86 \
+--filter-sample-max-read-depth 344 \
+--window-average-policy valid-loci \
+--filter-total-snp-min-frequency 0.01 \
+--filter-total-only-biallelic-snps \
+--pool-sizes 60 \
+--file-prefix all_samples_fst_calculation_ \
+--out-dir /home/tomsch/WGS_36/aligned_new/fst_files/all_samples \
+--compress \
+--log-file /home/tomsch/WGS_36/aligned_new/fst_files/all_samples/all_samples_fst.log \
 --threads 20
 
 # just for the mitochondrium 
